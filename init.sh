0eb8a8ed2500234fafc4dd9c8904056535b75a00
AnterCreeper <wangzhihao9@yeah.net>
2025-12-11 00:32:59 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
+# configuration
 major=0.18.2
 minor=1.1.0
 version=1.21.10
-jarfile=fabric-server-mc.$version-loader.$major-launcher.$minor.jar
-eula=3  #line number of eula=false
-modlist="modlist.txt"
-cfglist="cfglist.txt"
-xmxsize=6144
-cmdline="-Xdisableexplicitgc -Xgcpolicy:balanced -XX:+UseVectorCmov -XX:+PerfDisableSharedMem -XX:-DontCompileHugeMethods -XX:+AlwaysActAsServerClassMachine -XX:+AlwaysPreTouch"
 
+eula=3                 #the line number of "eula=false"
+modlist="modlist.txt"  #url list of mods
+cfglist="cfglist.txt"  #modify list of server.properties
+xmxsize=6144           #Xmx size in megabytes
+
+source=https://meta.fabricmc.net/v2/versions/loader/$version/$major/$minor/server/jar
+jarfile=fabric-server-mc.$version-loader.$major-launcher.$minor.jar
+cmdline="-XX:+UseZGC -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:+PerfDisableSharedMem -XX:-DontCompileHugeMethods"
+
+# switch to script root
 script_dir=$(cd "$(dirname "$0")" && pwd)
 cd "${script_dir}"
 
+# run script if exist
 launch() {
-screen -dmS minecraft -s ./run.sh
+screen -DmS minecraft -s ./run.sh
 }
-
 if [ -f "run.sh" ]; then
 launch
+exit 0
 fi
 
 {
@@ -25,18 +31,18 @@ fi
 apt update
 apt install -y screen aria2
 
-# download mods
+# download mod jarfiles
 mkdir mods
 cd mods
 while read file
 do
 aria2c -x 16 -s 16 $file
 done < ../${modlist}
-
 } &
+
 {
 # download minecraft jarfile
-curl -OJ https://meta.fabricmc.net/v2/versions/loader/$version/$major/$minor/server/jar
+curl -OJ $source
 while [ ! -f "eula.txt" ]
 do
 java -jar $jarfile nogui
@@ -45,18 +51,16 @@ done
 # setup eula
 sed -i "${eula}s/false/true/" eula.txt
 
-# setup config file
+# setup server.properties
 while read args
 do
 IFS=',' read -ra arg <<< "${args}"
 sed -i "s/^${arg[0]}=.*/${arg[0]}=${arg[1]}/" server.properties
 done < $cfglist
-
 } &
-wait
+
 # setup run script
-nursery_minimum=$(($xmxsize/4))
-nursery_maximum=$(($xmxsize*2/5))
-echo "java ${cmdline} -Xmx${xmxsize}M -Xms512M -Xmnx${nursery_maximum}M -Xmns${nursery_minimum}M -jar ${jarfile} nogui" > run.sh
+wait
+echo "java ${cmdline} -Xmx${xmxsize}M -Xms512M -jar ${jarfile} nogui" > run.sh
 chmod +x run.sh
 launch
